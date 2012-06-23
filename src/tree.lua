1b3ec9b8d7d2034cebd438da1352d379c358e7b9
Nikolai Rubashov <nikolai.salmanovich@gmail.com>
2012-06-24 00:22:35 +0900

    Refactor.

--- src/tree.lua
+++ src/tree.lua
@@ -12,30 +12,34 @@ function Tree:new()
   return o
 end
 
-function Tree.ingest_string(tree, word)
+-- TODO: UTF-8!
+function Tree.insert(tree, word)
+  if tree.tree then tree = tree.tree end
+
   local lg = word:len()
   if lg > 0 then
     local head, tail = word:sub(1, 1), word:sub(2, word:len())
 
     tree[head] = tree[head] or { }
-    Tree.ingest_string(tree[head], tail)
+    Tree.insert(tree[head], tail)
   else
     tree[0] = ''
   end
 end
 
--- TODO: UTF-8!
-function Tree:ingest(word)
-  if type(word) == 'table' then
-    for _, w in ipairs(word) do
-      self:ingest(w)
+function Tree.ingest(tree, words)
+  if type(words) == 'table' then
+    for _, word in ipairs(words) do
+      Tree.insert(tree, word)
     end
-  elseif type(word) == 'string' then
-    Tree.ingest_string(self.tree, word)
+  elseif type(word) == 'string' then -- ‘words’ is a single word after all.
+    Tree.insert(tree, words)
   end
 end
 
-function Tree.tree_size(tree)
+function Tree.size(tree)
+  if tree.tree then tree = tree.tree end
+
   local n = 0
 
   for head, tail in pairs(tree) do
@@ -43,7 +47,7 @@ function Tree.tree_size(tree)
     if head == 0 then
       s = 1
     else
-      s = Tree.tree_size(tail)
+      s = Tree.size(tail)
     end
 
     n = n + s
@@ -52,40 +56,36 @@ function Tree.tree_size(tree)
   return n
 end
 
-function Tree:size()
-  return Tree.tree_size(self.tree)
-end
+function Tree.dump(tree, leader, words)
+  if tree.tree then
+    tree = tree.tree
+    leader = ''
+    words = { }
+  end
 
-function Tree.tree_dump(leader, tree, words)
   for head, tail in pairs(tree) do
     if head ~= 0 then
-      Tree.tree_dump(leader .. head, tail, words)
+      Tree.dump(tail, leader .. head, words)
     else
       table.insert(words, leader)
     end
   end
-end
 
-function Tree:dump()
-  words = { }
-  Tree.tree_dump('', self.tree, words)
   return words
 end
 
-function Tree.tree_delete(tree, word)
+function Tree.delete(tree, word)
+  if tree.tree then tree = tree.tree end
+
   if word ~= '' then
     local head, tail = word:sub(1, 1), word:sub(2, word:len())
     local t = tree[head]
     if t then
-      Tree.tree_delete(t, tail)
+      Tree.delete(t, tail)
     end
   else
     if tree[0] == '' then
       tree[0] = nil
     end
   end
-end
-
-function Tree:delete(word)
-  Tree.tree_delete(self.tree, word)
 end
