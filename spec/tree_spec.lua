6d1705a27739a46262b89640f3f14e480ace4c71
Nikolai Rubashov <nikolai.salmanovich@gmail.com>
2013-07-11 06:29:33 +0900

    One additional test for safety.

--- spec/tree_spec.lua
+++ spec/tree_spec.lua
@@ -105,5 +105,6 @@ describe['A tree'] = function()
     tree:ingest('a1ic', 'l1g2', 'e1b', 'br4', 'e2br', '2ai2', 'eb1ra')
     tree:insert('4ai.')
     expect(table.is_equal(tree:matches('algebraic'), { 'lg', 'ebra', 'ebr', 'eb', 'br', 'aic', 'ai' })).should_be(true)
+    expect(table.is_equal(tree:matches('Dai'), { 'ai', 'ai.' })).should_be(true)
   end
 end
