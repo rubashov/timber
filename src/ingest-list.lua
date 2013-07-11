0100ffbd46edb3581d2cd67710c6f58f789712d8
Nikolai Rubashov <nikolai.salmanovich@gmail.com>
2013-07-11 11:18:07 +0900

    Enhanced ingestion script.

--- src/ingest-list.lua
+++ src/ingest-list.lua
@@ -7,8 +7,30 @@ local field = (1 - semicolon)^1
 local linepatt = C(field) * semicolon
 
 tree = Tree:new()
-for line in io.lines(arg[1]) do
-  local word = lpeg.match(linepatt, line)
-  tree:ingest(word)
-  print('Ingested ' .. word)
+
+function ingest(filename, silent)
+  local n = 0
+  for line in io.lines(filename) do
+    local word = lpeg.match(linepatt, line)
+    tree:ingest(word)
+    if not silent then
+      print('Ingested ' .. word)
+    end
+    n = n + 1
+  end
+
+  print('Ingested ' .. tostring(n) .. ' words')
 end
+
+function main(arg)
+  if arg[1] == '-q' then
+    silent = true
+    path = arg[2]
+  else
+    path = arg[1]
+  end
+
+  ingest(path, silent)
+end
+
+main(arg)
